6f9b3d0d3040bbcddce4c9bda581471fff3ec2ed
okozlova <okozlova@mail.uni-paderborn.de>
2020-04-30 16:09:04 +0200

    created list for categories

---
+++ src/sidebarList/SidebarList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './sidebarList.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+function SidebarList(props) {
+    const categories = props.categories;
+    const listItems = categories.map((category) =>
+        <ListItem categoryName={category.title} icon={category.icon} />
+    );
+    return(
+        <div>
+            {listItems}
+        </div>
+    );
+}
+
+function ListItem(props) {
+    return(
+        <div className="list-item">
+            <FontAwesomeIcon className="icon" icon={props.icon} />
+            <span className="category-text" >
+                {props.categoryName}
+            </span>
+        </div>
+    );
+}
+
+export default SidebarList;
